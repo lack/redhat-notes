59a6afa55d8ea5c050fb43c90e61ad97278818a3
Jim Ramsay <jramsay@redhat.com>
2021-06-05 06:24:56 +0000

    Make the generator's tang servers configurable

--- rekeying/generator.sh
+++ rekeying/generator.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-declare -A TANG_SERVERS
-TANG_SERVERS['http://10.46.55.192:7500']='aweILXiRhPQoVUP37pwUA5RFThM'
-TANG_SERVERS['http://10.46.55.192:7501']='I5Ynh2JefoAO3tNH9TgI4obIaXI'
-TANG_SERVERS['http://10.46.55.192:7502']='38qWZVeDKzCPG9pHLqKzs6k1ons'
+log() {
+  echo "$@" >&2
+}
+
+declare -A tang_servers
+if [[ $TANG_SERVERS ]]; then
+  servers=($TANG_SERVERS)
+  i=0
+  while [[ $i -lt ${#servers[*]} ]]; do
+    url=${servers[((i++))]}
+    thp=${servers[((i++))]}
+    tang_servers[$url]="$thp"
+    log "Added tang server $url ($thp)"
+  done
+else
+  log "Using example servers"
+  log 'To specify others, export TANG_SERVERS="http://tangserver01:7500 thumbprint1 ..."'
+  tang_servers['http://tangserver01:7500']='tang-thumbprint-1'
+  tang_servers['http://tangserver02:7500']='tang-thumbprint-2'
+  tang_servers['http://tangserver03:7500']='tang-thumbprint-3'
+fi
 
 indented_list() {
   if [[ $# -le 1 ]]; then
@@ -33,8 +50,8 @@ tang_config() {
 
 tang_pin() {
   local tang_configs=()
-  for server in "${!TANG_SERVERS[@]}"; do
-    tang_configs+=("$(tang_config "$server" "${TANG_SERVERS[$server]}")")
+  for server in "${!tang_servers[@]}"; do
+    tang_configs+=("$(tang_config "$server" "${tang_servers[$server]}")")
   done
   printf '"tang":['
   indented_list "${tang_configs[@]}"
@@ -70,6 +87,7 @@ write_yaml() {
   local pin_name="$2"
   local pin="$3"
   generate_yaml "$type" "$pin_name" "$pin" > $type-$pin_name.yaml
+  log "Generated $type-$pin_name.yaml"
 }
 
 declare -A pins
