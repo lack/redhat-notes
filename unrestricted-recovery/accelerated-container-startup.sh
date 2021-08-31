f912d7243003c7867ccda5973c12b18b60107116
Jim Ramsay <jramsay@redhat.com>
2021-08-31 13:14:30 -0400

    Updated logging and configuration for accelerated-container-startup
    This simplifies the logging (with less calls to the kernel to check
    state), and removes the need for a path watcher (this can start
    independently of kubelet now)

--- unrestricted-recovery/accelerated-container-startup.sh
+++ unrestricted-recovery/accelerated-container-startup.sh
@@ -13,9 +13,11 @@ MAXIMUM_WAIT_TIME=${MAXIMUM_WAIT_TIME:-600}
 
 unrestrictedCpuset() {
   if [[ ! -e /var/lib/kubelet/cpu_manager_state ]]; then
-    return 1
+    # use all the cpus if kubelet is not configured yet
+    cat /sys/fs/cgroup/cpuset/cpuset.cpus
+  else
+    jq -r '.defaultCpuSet' </var/lib/kubelet/cpu_manager_state
   fi
-  jq -r '.defaultCpuSet' </var/lib/kubelet/cpu_manager_state
 }
 
 restrictedCpuset() {
@@ -28,25 +30,12 @@ restrictedCpuset() {
   return 1
 }
 
-logAffinity() {
-  echo "----------------------------------------------------------"
-  for proc in $CRITICAL_PROCESSES; do
-    echo "- $proc"
-    local pids="$(pgrep $proc)"
-    for pid in $pids; do
-      taskset -p $pid | sed -e 's/^/  - /'
-    done
-  done
-}
-
 resetAffinity() {
   local cpuset="$1"
   local failcount=0
   local successcount=0
-  echo "=========================================================="
-  echo "Setting CPU affinity for critical processes \"$CRITICAL_PROCESSES\" to $cpuset"
+  logger "Recovery: Setting CPU affinity for critical processes \"$CRITICAL_PROCESSES\" to $cpuset"
   for proc in $CRITICAL_PROCESSES; do
-    echo "- $proc"
     local pids="$(pgrep $proc)"
     for pid in $pids; do
       local tasksetOutput
@@ -57,23 +46,22 @@ resetAffinity() {
       else
         ((successcount++))
       fi
-      taskset -p $pid | sed -e 's/^/  - /'
     done
   done
-  echo "Re-affined $successcount pids successfully"
+  logger "Recovery: Re-affined $successcount pids successfully"
   if [[ $failcount -gt 0 ]]; then
-    echo "Failed to re-affine $failcount processes"
+    logger "Recovery: Failed to re-affine $failcount processes"
     return 1
   fi
 }
 
 setUnrestricted() {
-  logger "Setting critical system processes to have unrestricted CPU access"
+  logger "Recovery: Setting critical system processes to have unrestricted CPU access"
   resetAffinity "$(unrestrictedCpuset)"
 }
 
 setRestricted() {
-  logger "Resetting critical system processes back to normally restricted access"
+  logger "Recovery: Resetting critical system processes back to normally restricted access"
   resetAffinity "$(restrictedCpuset)"
 }
 
@@ -83,38 +71,33 @@ currentAffinity() {
 }
 
 waitForReady() {
-  echo "=========================================================="
-  echo "Waiting ${MAXIMUM_WAIT_TIME}s for the initialization to complete"
+  logger "Recovery: Waiting ${MAXIMUM_WAIT_TIME}s for the initialization to complete"
   local lastSystemdCpuset="$(currentAffinity 1)"
   local lastDesiredCpuset="$(unrestrictedCpuset)"
   local t=0 s=10
   while [[ $t -lt $MAXIMUM_WAIT_TIME ]]; do
-    # TODO: Can we query the state somehow and interrupt this sooner?
-    # Does 'crio' know when we're "done"?  Maybe via 'crictl ps | grep Pending | wc -l'?
-    local pending=$(crictl ps | grep Pending | wc -l)
-    echo "DEBUG: Crio lists $pending containers as Pending"
     sleep $s
     ((t += s))
+    # Re-check the current affinity of systemd, in case some other process has changed it
     local systemdCpuset="$(currentAffinity 1)"
+    # Re-check the unrestricted Cpuset, as the allowed set of unreserved cores may change as pods are assigned to cores
     local desiredCpuset="$(unrestrictedCpuset)"
-    # Because 'tuned' may mess with us, and the allowed set of unreserved
-    # cores may change as pods come up, we need to reassert the unrestricted
-    # affinity if it's not correct
     if [[ $systemdCpuset != $lastSystemdCpuset || $lastDesiredCpuset != $desiredCpuset ]]; then
       resetAffinity "$desiredCpuset"
       lastSystemdCpuset="$(currentAffinity 1)"
       lastDesiredCpuset="$desiredCpuset"
     fi
   done
+  logger "Recovery: Recovery Complete Timeout"
 }
 
 if ! unrestrictedCpuset >&/dev/null; then
-  echo "No unrestricted Cpuset could be detected.  Perhaps kubelet is not running yet?"
+  logger "Recovery: No unrestricted Cpuset could be detected"
   exit 1
 fi
 
 if ! restrictedCpuset >&/dev/null; then
-  echo "No restricted Cpuset has been configured.  We are already running unrestricted."
+  logger "Recovery: No restricted Cpuset has been configured.  We are already running unrestricted."
   exit 0
 fi
 
@@ -123,5 +106,6 @@ fi
 # via ^C or SIGTERM, we return things back to the way they should be.
 trap setRestricted EXIT
 
+logger "Recovery: Recovery Mode Starting"
 setUnrestricted
 waitForReady
